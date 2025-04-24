f367a2b763698b583df6d94000f31aaae3c082db
Michael Kim <michael.kim.200518@gmail.com>
2025-04-24 15:11:40 +0900

    chore(deps): update boa-contracts-lib to ^1.0.7
    - Update boa-contracts-lib dependency from ^1.0.5 to ^1.0.7 in packages/client
    - Update yarn.lock accordingly
    - Bump package version to 1.0.3

--- packages/client/test/helper/constants.ts
+++ packages/client/test/helper/constants.ts
@@ -2,6 +2,7 @@ import { ContextParams } from "../../src";
 
 import { Wallet } from "@ethersproject/wallet";
 
+// 0x9630fF452211Cc95BBFa32c0C4cF68eB498b8549
 export const TEST_WALLET = "c48891d685be9c531b592f66da2988dc2f1e1eb83f0e6de8d9a483be221d8376";
 
 export const contextParamsMainnet: ContextParams = {
